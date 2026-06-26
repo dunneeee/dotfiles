4538a971092d80a1969e55ef2baa0449ad8918d2
Dung Le The <corepper03@gmail.com>
2026-06-26 22:41:32 +0700

    feat(nvim): add spell checker

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,13 @@
 return {
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-    "neovim/nvim-lspconfig",
-    },
-    opts = {
-      ensure_installed = { "lua_ls", "tailwindcss", "vtsls", "taplo", "jsonls" },
-      automatic_installation = true,
-    }
-  },
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = { "lua_ls", "tailwindcss", "vtsls", "taplo", "jsonls", "codebook" },
+			automatic_installation = true,
+		},
+	},
 }
