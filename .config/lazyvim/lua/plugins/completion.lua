53567585ecdf8f7f4c032169697f1bf43298d7e4
Dung Le The <corepper03@gmail.com>
2026-06-27 11:45:11 +0700

    feat(nvim): lazyvim with custom config

---
+++ .config/lazyvim/lua/plugins/completion.lua
@@ -0,0 +1,16 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = { preselect = true, auto_insert = false },
+      },
+
+      ghost_text = {
+        enabled = true,
+      },
+
+      accept = { auto_brackets = { enabled = false } },
+    },
+  },
+}
